0734993936065de8037562a2c579be49ded1daee
jeffersoncustodio820 <jeffersoncustodio820@gmail.com>
2018-11-15 17:21:15 -0200

    criacao corpo pag home

--- js/Index.js
+++ js/Index.js
@@ -1,14 +1,24 @@
-$("#button-open-menu").click(function () {
-    $(".div-list").fadeToggle(1000);
-    $(".fa-bars").toggleClass("fa-times");
-});
-$(window).load("this", function () {
+
+window.addEventListener("load", function () {
     $(".div-wrap-loading").delay(2000).fadeOut(1000);
-    $(".nav-bar").addClass('on');
-    $(".brand-heading-center-ap").addClass('on');
-    $(".button-acessar-conta").addClass('on');
+    $(".nav-bar, .brand-heading-center-ap, .button-acessar-conta").addClass('on');
     setInterval(() => {
        $('body').css('overflow', 'auto') 
     }, 3000);
 });
-    
+
+$("#button-open-menu").click(function () {
+    $(".div-list").fadeToggle(1000);
+    $(".fa-bars").toggleClass("fa-times");
+});    
+
+$('#icon-scroll-top').click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+});
+
+$('#contatos').click(function(){
+    $('html, body').animate({scrollTop : $("footer").offset().top},1500);
+    return false;
+});
+
